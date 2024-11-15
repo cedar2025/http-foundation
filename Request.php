ea46a85db35fefe776ccd1a298ff3cade89f3e55
xboard <xboard@xboard.one>
2024-11-15 17:20:24 +0800

    init

--- Request.php
+++ Request.php
@@ -1517,7 +1517,8 @@ class Request
         }
 
         if (null === $this->content || false === $this->content) {
-            $this->content = file_get_contents('php://input');
+            $php_input = file_get_contents('php://input');
+            $this->content = empty($php_input) ? json_encode($_POST) : $php_input;
         }
 
         return $this->content;
